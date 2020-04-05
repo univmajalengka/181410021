56edbef93efa6e9d43a8aa29851cc4615db4a049
sandramarliantimulyana <50065176+sandramarliantimulyana@users.noreply.github.com>
2020-04-05 20:40:44 +0700

    Update Database

--- SBDL/koperasi.sql
+++ SBDL/koperasi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 03 Apr 2020 pada 10.47
+-- Waktu pembuatan: 05 Apr 2020 pada 15.34
 -- Versi server: 10.1.37-MariaDB
 -- Versi PHP: 7.3.1
 
@@ -19,8 +19,28 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `koperasi_new`
+-- Database: `koperasi`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`id_user`, `username`, `password`) VALUES
+(1, 'admin', 'd41d8cd98f00b204e9800998ecf8427e'),
+(2, 'user', 'd41d8cd98f00b204e9800998ecf8427e');
 
 -- --------------------------------------------------------
 
@@ -29,56 +49,90 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `data_anggota` (
-  `username` varchar(16) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `nama` varchar(20) NOT NULL,
-  `nik` bigint(16) NOT NULL,
-  `tgl_lahir` date NOT NULL,
-  `jenis_kelamin` varchar(9) NOT NULL,
-  `pekerjaan` varchar(16) NOT NULL,
-  `alamat` text NOT NULL,
-  `email` text NOT NULL,
-  `no_hp` char(20) NOT NULL,
-  `tabungan` varchar(16) NOT NULL,
-  `pinjaman` varchar(16) NOT NULL
+  `no_anggota` int(11) NOT NULL,
+  `nama_anggota` varchar(30) NOT NULL,
+  `tempat_lahir` varchar(30) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `jenis_kelamin` varchar(1) NOT NULL,
+  `alamat` varchar(30) NOT NULL,
+  `no_telepon` varchar(20) NOT NULL,
+  `status` varchar(15) NOT NULL,
+  `total_simpanan` int(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `data_anggota`
 --
 
-INSERT INTO `data_anggota` (`username`, `password`, `nama`, `nik`, `tgl_lahir`, `jenis_kelamin`, `pekerjaan`, `alamat`, `email`, `no_hp`, `tabungan`, `pinjaman`) VALUES
-('aldi', '202cb962ac59075b964b07152d234b70', 'Aldi Ardiansyah', 321111099876, '2000-04-02', 'L', 'Mahasiswa', 'Heuleut', 'aldi@gmail', '085155113198', '90000', '150000'),
-('hikayat', '202cb962ac59075b964b07152d234b70', 'Muhamad Hikayat', 321433566432, '1998-07-13', 'L', 'Mahasiswa', 'Pataking', 'hikayat@gmail', '08112384732', '900000', '800000'),
-('irna', '9fc3d7152ba9336a670e36d0ed79bc43', 'Irna Sri Nulingga', 321010100011, '2000-10-22', 'P', 'Mahasiswa', 'Weragati', 'irna@gmail.com', '089766566789', '400000', '400000'),
-('rizki', '202cb962ac59075b964b07152d234b70', 'Rizki Alam', 321345654001, '1999-01-01', 'L', 'Mahasiswa', 'Panyingkiran', 'rizki@gmail', '089766566789', '5000000', '500000'),
-('user', 'ee11cbb19052e40b07aac0ca060c23ee', 'User', 321010100012, '1993-04-16', 'L', 'Mahasiswa', 'mjlk', 'user@gmail.com', '08112384200', '400000', '300000');
+INSERT INTO `data_anggota` (`no_anggota`, `nama_anggota`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `alamat`, `no_telepon`, `status`, `total_simpanan`) VALUES
+(1, 'Sandra', 'Majalengka', '2019-11-01', 'P', 'Heuleut', '089797611492', 'Mahasiswa', 15000),
+(2, 'User', 'Majalengka', '2020-04-07', 'L', 'mjlk', '089797612000', 'Mahasiswa', 90000),
+(3, 'Marlianti', 'Majalengka', '2019-12-06', 'P', 'Heuleut', '089766544322', 'Menikah', 1000000),
+(4, 'Mulyana', 'Subang', '2019-11-08', 'L', 'Weragati', '089766544321', 'Menikah', 400000),
+(5, 'Anisah', 'Majalengka', '2019-08-10', 'P', 'Panyingkiran', '089766544123', 'Mahasiswa', 1000000);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `data_pembayaran`
+-- Struktur dari tabel `data_anggota_keluar`
 --
 
-CREATE TABLE `data_pembayaran` (
-  `username` varchar(20) NOT NULL,
-  `nama` varchar(20) NOT NULL,
-  `tgl_bayar` date NOT NULL,
-  `jml_bayar` int(20) NOT NULL
+CREATE TABLE `data_anggota_keluar` (
+  `tanggal_keluar` date NOT NULL,
+  `no_anggota` int(11) NOT NULL,
+  `nama_anggota` text NOT NULL,
+  `tempat_lahir` text NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `jenis_kelamin` varchar(1) NOT NULL,
+  `alamat` varchar(30) NOT NULL,
+  `no_telepon` varchar(20) NOT NULL,
+  `status` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `data_pembayaran`
+-- Dumping data untuk tabel `data_anggota_keluar`
 --
 
-INSERT INTO `data_pembayaran` (`username`, `nama`, `tgl_bayar`, `jml_bayar`) VALUES
-('member01', 'Ahmed ahmed', '2015-01-01', 50000),
-('sandra', 'sandra', '2020-01-01', 500000),
-('user', 'User', '2015-01-01', 200000),
-('rizki', 'Rizki Alam', '2020-02-20', 500000),
-('irna', 'Irna Sri Nulingga', '2020-05-10', 100000),
-('hikayat', 'Muhamad Hikayat', '2020-04-01', 200000),
-('aldi', 'Aldi Ardiansyah', '2020-05-01', 50000);
+INSERT INTO `data_anggota_keluar` (`tanggal_keluar`, `no_anggota`, `nama_anggota`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `alamat`, `no_telepon`, `status`) VALUES
+('2020-04-30', 12, 'Marlianti', 'Majalengka', '2019-11-08', 'P', 'Weragati', '085322233858', 'Menikah'),
+('2020-04-30', 13, 'Agus Mulyana', 'Majalengka', '2019-08-07', 'L', 'Heuleut', '085233123858', 'Menikah'),
+('2020-04-30', 14, 'Siti Anisah', 'Heuleut', '2019-07-10', 'P', 'Heuleut', '085323143112', 'Menikah'),
+('2020-04-30', 15, 'Tika', 'Majalengka', '2020-02-04', 'P', 'Heuleut', '085797611492', 'Pelajar'),
+('2020-04-10', 19, 'ijah', 'Majalengka', '2020-02-04', 'P', 'Palasah', '089797611492', 'Menikah');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_anggota_masuk`
+--
+
+CREATE TABLE `data_anggota_masuk` (
+  `tanggal_masuk` date NOT NULL,
+  `no_anggota` int(11) NOT NULL,
+  `nama_anggota` varchar(30) NOT NULL,
+  `tempat_lahir` varchar(30) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `jenis_kelamin` varchar(1) NOT NULL,
+  `alamat` varchar(30) NOT NULL,
+  `no_telepon` varchar(20) NOT NULL,
+  `status` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `data_anggota_masuk`
+--
+
+INSERT INTO `data_anggota_masuk` (`tanggal_masuk`, `no_anggota`, `nama_anggota`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `alamat`, `no_telepon`, `status`) VALUES
+('2020-04-01', 1, 'Sandra', 'Majalengka', '2019-11-01', 'P', 'Heuleut', '089797611492', 'Mahasiswa'),
+('2020-04-01', 2, 'User', 'Majalengka', '2020-04-07', 'L', 'mjlk', '089797612000', 'Mahasiswa'),
+('2020-04-01', 3, 'Marlianti', 'Majalengka', '2019-12-06', 'P', 'Heuleut', '089766544321', 'Menikah'),
+('2020-04-01', 4, 'Mulyana', 'Subang', '2019-11-08', 'L', 'Weragati ', '089766544321', 'Menikah'),
+('2020-04-01', 5, 'Anisah', 'Majalengka', '2019-08-10', 'P', 'Panyingkiran', '089766544123', 'Mahasiswa'),
+('2020-04-02', 12, 'Marlianti', 'Majalengka', '2019-11-08', 'P', 'Weragati', '085322233858', 'Menikah'),
+('2020-04-10', 13, 'Agus Mulyana', 'Majalengka', '2019-08-07', 'L', 'Heuleut', '085233123858', 'Menikah'),
+('2020-04-24', 14, 'Siti Anisah', 'Majalengka', '2019-07-10', 'P', 'Heuleut', '085323143112', 'Menikah'),
+('2020-04-01', 15, 'Tika', 'Majalengka', '2020-02-04', 'P', 'Heuleut', '086797611492', 'Pelajar'),
+('2020-04-01', 19, 'Ijah', 'Majalengka', '2020-02-04', 'P', 'Palasah', '089797611492', 'Menikah');
 
 -- --------------------------------------------------------
 
@@ -87,60 +141,23 @@ INSERT INTO `data_pembayaran` (`username`, `nama`, `tgl_bayar`, `jml_bayar`) VAL
 --
 
 CREATE TABLE `data_penarikan` (
-  `username` varchar(20) NOT NULL,
-  `nama` varchar(20) NOT NULL,
-  `tgl_ambil` date NOT NULL,
-  `jml_ambil` int(20) NOT NULL
+  `no_transaksi` int(30) NOT NULL,
+  `tanggal_penarikan` date NOT NULL,
+  `no_anggota` int(11) NOT NULL,
+  `nama_anggota` varchar(30) NOT NULL,
+  `jumlah_penarikan` int(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `data_penarikan`
 --
 
-INSERT INTO `data_penarikan` (`username`, `nama`, `tgl_ambil`, `jml_ambil`) VALUES
-('member01', 'Ahmed ahmed', '2015-01-01', 100000),
-('sandra', 'sandra', '2020-01-01', 200000),
-('sandra', 'sandra', '2020-12-01', 300000),
-('irna', 'Irna Sri Nulingga', '2020-02-01', 200000),
-('hikayat', 'Muhamad Hikayat', '2020-05-19', 50000),
-('afif', 'Afif Surya R', '2020-06-18', 200000),
-('afif', 'Afif Surya R', '2015-01-01', 200000),
-('user', 'User', '2020-02-01', 100000),
-('rizki', 'Rizki Alam', '2020-03-01', 5000000),
-('irna', 'Irna Sri Nulingga', '2020-05-01', 100000),
-('hikayat', 'Muhamad Hikayat', '2020-04-15', 100000),
-('aldi', 'Aldi Ardiansyah', '2020-07-19', 10000);
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `data_pinjaman`
---
-
-CREATE TABLE `data_pinjaman` (
-  `username` varchar(20) NOT NULL,
-  `nama` varchar(20) NOT NULL,
-  `tgl_transaksi` date NOT NULL,
-  `jml_transaksi` int(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `data_pinjaman`
---
-
-INSERT INTO `data_pinjaman` (`username`, `nama`, `tgl_transaksi`, `jml_transaksi`) VALUES
-('member01', 'Ahmed ahmed', '2015-01-01', 225000),
-('member01', 'Ahmed ahmed', '2015-01-01', 75000),
-('sandra', 'sandra', '2019-01-01', 1000000),
-('sandra', 'sandra', '2019-01-01', 40000),
-('irna', 'Irna Sri Nulingga', '2015-01-01', 100000),
-('hikayat', 'Muhamad Hikayat', '2020-05-01', 200000),
-('afif', 'Afif Surya R', '2020-06-01', 100000),
-('user', 'User', '2015-01-01', 1000000),
-('rizki', 'Rizki Alam', '2020-01-10', 1000000),
-('irna', 'Irna Sri Nulingga', '2020-01-20', 500000),
-('hikayat', 'Muhamad Hikayat', '2020-02-21', 1000000),
-('aldi', 'Aldi Ardiansyah', '2020-03-01', 200000);
+INSERT INTO `data_penarikan` (`no_transaksi`, `tanggal_penarikan`, `no_anggota`, `nama_anggota`, `jumlah_penarikan`) VALUES
+(6, '2020-04-01', 1, 'Sandra', 5000),
+(7, '2020-04-02', 2, 'User', 10000),
+(8, '2020-04-03', 3, 'Marlianti', 200000),
+(9, '2020-04-04', 4, 'Mulyana', 100000),
+(10, '2020-04-05', 5, 'Anisah', 100000);
 
 -- --------------------------------------------------------
 
@@ -149,70 +166,74 @@ INSERT INTO `data_pinjaman` (`username`, `nama`, `tgl_transaksi`, `jml_transaksi
 --
 
 CREATE TABLE `data_tabungan` (
-  `username` varchar(20) NOT NULL,
-  `nama` varchar(20) NOT NULL,
-  `tgl_tabungan` date NOT NULL,
-  `jml_tabungan` int(20) NOT NULL
+  `no_transaksi` int(30) NOT NULL,
+  `tanggal_simpanan` date NOT NULL,
+  `no_anggota` int(11) NOT NULL,
+  `nama_anggota` varchar(30) NOT NULL,
+  `jumlah_simpanan` int(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `data_tabungan`
 --
 
-INSERT INTO `data_tabungan` (`username`, `nama`, `tgl_tabungan`, `jml_tabungan`) VALUES
-('member01', 'Ahmed ahmed', '2015-01-01', 500000),
-('member01', 'Ahmed ahmed', '2015-01-01', 250000),
-('sandra', 'sandra', '2015-01-01', 500000),
-('sandra', 'sandra', '2020-05-01', 5000000),
-('sandra', 'sandra', '2020-06-17', 5000000),
-('user', 'User', '2020-01-01', 500000),
-('rizki', 'Rizki Alam', '2020-01-20', 5000000),
-('rizki', 'Rizki Alam', '2020-01-20', 5000000),
-('irna', 'Irna Sri Nulingga', '2020-04-15', 500000),
-('hikayat', 'Muhamad Hikayat', '2020-03-01', 1000000),
-('aldi', 'Aldi Ardiansyah', '2020-01-20', 100000);
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `login`
---
-
-CREATE TABLE `login` (
-  `username` varchar(15) NOT NULL,
-  `nama` varchar(30) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `hak_akses` varchar(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `login`
---
-
-INSERT INTO `login` (`username`, `nama`, `password`, `hak_akses`) VALUES
-('admin01', 'Admin', '202cb962ac59075b964b07152d234b70', 'Admin'),
-('aldi', 'Aldi Ardiansyah', '202cb962ac59075b964b07152d234b70', 'Member'),
-('hikayat', 'Muhamad Hikayat', '202cb962ac59075b964b07152d234b70', 'Member'),
-('irna', 'Irna Sri Nulingga', '9fc3d7152ba9336a670e36d0ed79bc43', 'Member'),
-('rizki', 'Rizki Alam', '202cb962ac59075b964b07152d234b70', 'Member'),
-('user', 'User', 'ee11cbb19052e40b07aac0ca060c23ee', 'Member');
+INSERT INTO `data_tabungan` (`no_transaksi`, `tanggal_simpanan`, `no_anggota`, `nama_anggota`, `jumlah_simpanan`) VALUES
+(1, '2020-03-11', 1, 'Sandra', 20000),
+(2, '2020-03-13', 2, 'User', 100000),
+(3, '2020-03-22', 3, 'Marlianti', 1200000),
+(4, '2020-03-27', 4, 'Mulyana', 500000),
+(5, '2020-03-24', 5, 'Anisah', 1100000);
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `Username` (`username`);
+
+--
 -- Indeks untuk tabel `data_anggota`
 --
 ALTER TABLE `data_anggota`
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `nik` (`nik`);
+  ADD PRIMARY KEY (`no_anggota`);
 
 --
--- Indeks untuk tabel `login`
+-- Indeks untuk tabel `data_anggota_keluar`
 --
-ALTER TABLE `login`
-  ADD PRIMARY KEY (`username`);
+ALTER TABLE `data_anggota_keluar`
+  ADD PRIMARY KEY (`no_anggota`);
+
+--
+-- Indeks untuk tabel `data_anggota_masuk`
+--
+ALTER TABLE `data_anggota_masuk`
+  ADD PRIMARY KEY (`no_anggota`);
+
+--
+-- Indeks untuk tabel `data_penarikan`
+--
+ALTER TABLE `data_penarikan`
+  ADD PRIMARY KEY (`no_anggota`);
+
+--
+-- Indeks untuk tabel `data_tabungan`
+--
+ALTER TABLE `data_tabungan`
+  ADD PRIMARY KEY (`no_anggota`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
